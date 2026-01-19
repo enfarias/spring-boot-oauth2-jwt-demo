3b4db40d13a74af98b7f1e250133dbf4041420c8
Edson Ney de Farias <edson.ney@gmail.com>
2026-01-19 12:53:39 -0300

    Authorization server, Resource server, not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -35,7 +35,7 @@ public class UserService implements UserDetailsService {
         for (UserDetailsProjection projection : result) {
             user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
         }
-        
+
         return user;
     }
 
